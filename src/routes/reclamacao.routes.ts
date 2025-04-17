8259d22b5f65808be991e9552019b680c1bd98f0
Davy Ribeiro <davy.oliv.ribeiro@gmail.com>
2025-04-16 22:23:30 -0300

    feature: aicionando rotas para Reclamação. Criando e adicionando a primeira parte do Controller Reclamação

--- src/routes/reclamacao.routes.ts
+++ src/routes/reclamacao.routes.ts
@@ -1,5 +1,9 @@
 import  express from "express";
-
+import { getAllReclamacoes, getById, postReclamacao } from "../controllers/reclamacao.controller";
 const router = express.Router()
 
+router.get('/', getAllReclamacoes);
+router.get('/:id', getById)
+
+router.post('/', postReclamacao)
 export default router
