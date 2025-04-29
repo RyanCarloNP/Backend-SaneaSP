ff91b0647b01e098939fa853f36b8f359b264f53
devDavyRibeiro <davy.oliv.ribeiro@gmail.com>
2025-04-29 17:27:09 -0300

    criação das rotas de get

--- src/routes/reclamacao.routes.ts
+++ src/routes/reclamacao.routes.ts
@@ -1,9 +1,38 @@
-import  express from "express";
+import express, { Request, Response } from "express";
 import { getAllReclamacoes, getById, postReclamacao } from "../controllers/reclamacao.controller";
 const router = express.Router()
 
-router.get('/', getAllReclamacoes);
-router.get('/:id', getById)
+router.get('/', async (req: Request, res: Response) => {
+    try {
+        const foundReclamacoes = await getAllReclamacoes();
+        res.status(200).json(foundReclamacoes);
+    } catch (error) {
+        res.status(500).json({
+            error: true,
+            message: `Ocorreu um erro de servidor ${error} `,
+        });
+    }
+});
+router.get('/:id', async (req: Request, res: Response) => {
+    try {
+        const id = Number(req.params.id);
+        const reclamacao = await getById(id);
+        if (!reclamacao) {
+            res.status(404).json({
+                error: true,
+                message: "Não foi possível encontrar uma Reclamação com esse ID"
+            })
+        }
+
+        res.status(200).json(reclamacao);
+        return;
+    } catch (error) {
+        res.status(500).json({
+            error: true,
+            message: `Ocorreu um erro de servidor ${error} `,
+        });
+    }
+})
 
 router.post('/', postReclamacao)
 export default router
