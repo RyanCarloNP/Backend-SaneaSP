12734fdee6c4270600a82b675bee0565ce829315
Ryan <ryanjobs234@gmail.com>
2025-05-08 20:06:55 -0300

    Sêmi conclusão da controller de Notícias

--- src/controllers/noticia.controller.ts
+++ src/controllers/noticia.controller.ts
@@ -18,14 +18,87 @@ export const getNoticiaById = async (noticiaId: number) => {
     return noticia;
 }
 
-export const getNoticiaByName = async (noticiaName: string) => {
-    const noticia = await NoticiaModel.findOne({ where: { titulo: noticiaName } });
+export const getNoticiaByTitle = async (noticiaTitle: string) => {
+    const noticia = await NoticiaModel.findOne({ where: { titulo: noticiaTitle } });
     return noticia;
 }
 
-export const createNoticia = async (nome: string, descricao: string, dataPublicacao: Date,
-    objAdmin: number, objFonte: string[], objImagem: string[]): Promise<IApiResponse<INoticia>> => {
-    
-    const foundNoticia = await NoticiaModel.findOne({where:{[Op.like]: `${nome}`}});
-    
+export const createNoticia = async (newNoticia: INoticia): Promise<IApiResponse<INoticia>> => {
+    const noticiaFound = await NoticiaModel.findOne({ where: { titulo: { [Op.like]: `${newNoticia.titulo}` } } });
+
+    if (noticiaFound) {
+        return {
+            error: true,
+            message: "Está notícia já está cadastrada",
+            httpError: HttpError.Conflict
+        }
+    }
+
+    const createdNoticia = await NoticiaModel.create(newNoticia);
+    return {
+        error: false,
+        message: "Noticia cadastrada com sucesso",
+        data: createdNoticia
+    }
+}
+
+export const updateNoticia = async (noticiaData: INoticia): Promise<IApiResponse<INoticia>> => {
+    const noticiaFound = await NoticiaModel.findOne({ where: { id: noticiaData.id } });
+
+    if (noticiaFound == null) {
+        return {
+            error: true,
+            message: "Nenhuma noticia encontrada",
+            httpError: HttpError.NotFound
+        }
+    }
+
+    if (noticiaFound.titulo === noticiaData.titulo) {
+        return {
+            error: true,
+            message: "O titulo da noticia é igual ao seu titulo anterior",
+            httpError: HttpError.BadRequest
+        }
+    }
+
+    const noticiaTitleExists = await NoticiaModel.findOne({
+        where: {
+            titulo: { [Op.like]: `%${noticiaData.titulo}%` },
+            id: { [Op.ne]: noticiaData.id }
+        }
+    })
+
+    if (noticiaTitleExists) {
+        return {
+            error: true,
+            message: "Já existe uma noticia com esse titulo",
+            httpError: HttpError.Conflict
+        }
+    }
+
+    const updatedNoticia = await noticiaFound.update(noticiaData);
+    return {
+        error: false,
+        message: "Noticia atualizada com sucesso",
+        data: updatedNoticia
+    }
+}
+
+export const deleteNoticia = async (noticiaId: number): Promise<IApiResponse> => {
+    const noticiaFound = await NoticiaModel.findByPk(noticiaId)
+
+    if (!noticiaFound) {
+        return {
+            message: 'Nenhuma noticia foi encontrada',
+            error: true,
+            httpError: HttpError.NotFound
+        };
+    }
+
+    await noticiaFound.destroy();
+    return {
+        error: false,
+        message: "Noticia excluida com sucesso",
+        data: noticiaFound
+    }
 }
